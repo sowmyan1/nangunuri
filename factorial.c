8a3e2a4857254bfc2e45fee22b683ee6546d49da
sowmyan1 <49471342+sowmyan1@users.noreply.github.com>
2019-04-10 17:22:06 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int c, n=5, fact = 1;
+  
+  for (c = 1; c <= n; c++)
+    fact = fact * c;
+ 
+  printf("Factorial of %d = %d\n", n, fact);
+ 
+  return 0;
+}
